1a17ac35860835146f4cb70f7804e470b0f206f6
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-10-30 16:14:43 -0500

    Ejercicio de sonido

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let sonido;
+
+function preload() {
+  // soundFormats("wan", "mp3", "ogg");
+  sonidoBassloop = loadSound("sonido/bassloop.mp3");
 }
 
-function draw() {
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mousePressed(presionado);
+}
+
+function presionado() {
+  sonidoBassloop.play();
+  circle(mouseX, mouseY, 20);
+}
+
+function draw() {}
+
+function mouseDragged() {
+  sonidoBassloop.play();
   circle(mouseX, mouseY, 20);
 }
